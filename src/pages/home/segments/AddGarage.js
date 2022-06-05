c216f063df9c2621b5d5f8fa5eba1e9423728e94
Jon Graf <jongraf1@gmail.com>
2022-06-05 18:55:43 -0500

    added the AddGarage component and mostly got it working

---
+++ src/pages/home/segments/AddGarage.js
@@ -0,0 +1,97 @@
+import React, { useState, useContext } from 'react';
+import { View, Text } from 'react-native';
+import styles from './AddGarage.styles.js';
+import { Context } from '../../../state/Store';
+import { GreenButton } from '../../../components/controls/Buttons.js';
+import { MaterialIcons } from '@expo/vector-icons' 
+import { TextInput } from '@react-native-material/core';
+import { Checkbox } from 'react-native-paper';
+
+export default function AddGarage(props) {
+    const [state, dispatch] = useContext(Context);
+    const [succeeded, setSucceeded] = useState();
+    const [preferred, setPreferred] = useState(false);
+    const [garageName, setGarageName] = useState('');
+    const [isNameValid, setIsNameValid] = useState(true);
+    const [garageTouched, setGarageTouched] = useState(false);
+    const [availableNodes, setAvailableNodes] = useState(1);
+
+    const checkGarageName = (input) => {
+        if (availableNodes > 0) {
+            const name = input.target.value;
+            setGarageTouched(true);
+            setIsNameValid(name !== "");
+            setGarageName(name);
+        }
+    }
+
+    const submitGarageDoor = async (event) => {
+        event.preventDefault();
+        (garageTouched && isNameValid)
+            ? updateGarageNode()
+            : setIsNameValid(false);
+    }
+
+    const updateGarageNode = async () => {
+        // const response = await addUserDeviceNode(state.user.userId, state.auth.bearer, state.deviceId, garageName, preferred);
+        updateRoles();
+        setSucceeded(response.ok);
+        setPreferred(false);
+        const jsonResponse = await response.json();
+        setAvailableNodes(jsonResponse.availableNodes);
+        dispatch({ type: "SET_ADDED_GARAGE_NODE", payload: true })
+        if (jsonResponse.availableNodes === 0) {
+            props.close();
+        }
+    }
+
+    const updateRoles = async () => {
+        // const userRoles = await getRolesByUserId(state.user.userId, state.auth.bearer);
+        await dispatch({ type: 'SET_USER_DATA', payload: { ...state.user, roles: userRoles.roles } });
+        const garageRole = userRoles.roles.find(x => x.role_name === 'garage_door');
+        await dispatch({ type: 'SET_GARAGE_ROLE', payload: garageRole });
+    }
+
+    const resetDevices = () => {
+        setSucceeded(false);
+        setGarageName('');
+        setGarageTouched(false);
+    }
+
+    return (
+        <View>
+            {succeeded
+                ? <View>
+                    <View style={styles.deviceGroup}>
+                        <View style={styles.deviceGroup}>
+                            <View style={styles.borderSuccessIcon}>
+                                <MaterialIcons name='check_circle' style={styles.garageSuccessText}/>
+                                {/* <CheckCircle style={styles.garageSuccessText} /> */}
+                            </View>
+                            <Text style={styles.deviceText}>Successfully Added</Text>
+                        </View>
+                        <CloseIcon onPress={() => props.close()} style={styles.close-icon} />
+                    </View>
+                    <View style={styles.deviceRow}>
+                        <Text style={styles.deviceText}>Would you like to setup the remaining ({availableNodes}) openers?</Text>
+                    </View>
+                    <GreenButton onClick={resetDevices}>Add</GreenButton>
+                </View>
+                : <View>
+                    <View style={styles.deviceGroup}>
+                        <Text style={styles.deviceText}>Add Garage Door</Text>
+                    </View>
+                    <View>
+                        <View style={styles.accountRow}>
+                            <TextInput value={garageName} error={!isNameValid} onChange={checkGarageName} variant="outlined" label="Garage Name" />
+                        </View>
+                        <View style={styles.accountRow}>
+                            <Checkbox value={state.checkedG} onValueChange={() => setPreferred(!preferred)} name="checkedG" />
+                        </View>
+                        <GreenButton onPress={submitGarageDoor}>Add</GreenButton>
+                    </View>
+                </View>
+            }
+        </View>
+    );
+}
---
+++ src/pages/home/segments/AddGarage.styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+    garageSuccessText: {
+        color: '#055219',
+        alignSelf: 'center',
+    },
+
+    borderSuccessIcon: {
+        borderRadius: 40,
+        backgroundColor: 'white',
+        height: 1.5,
+        width: 1.5,
+        alignSelf: 'center',
+    },
+
+    deviceText: {
+        marginLeft: 1,
+    },
+
+    deviceGroup: {
+        // display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        // padding: 0 0 0 16,
+    },
+
+    deviceRow: {
+        // display: 'flex',
+        flexDirection: 'row',
+        marginTop: 0.5,
+    },
+});
