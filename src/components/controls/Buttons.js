b6ee81102405e7525791209d7b20ccc61ae710dc
Jon Graf <jongraf1@gmail.com>
2022-06-04 20:28:27 -0500

    converted the button components over to use in the app...default buttons fucking suck

---
+++ src/components/controls/Buttons.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import styles from './Buttons.styles';
+
+
+export function GreenButton(props) {
+    return (
+        <TouchableOpacity>
+            <Text type="submit" style={[styles.green, styles.myButton]} disabled={props.disabled} onClick={props.onPress}>{props.children}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export function BlueButton(props) {
+    return (
+        <TouchableOpacity>
+            <Text type="submit" style={[styles.blue, styles.myButton]} disabled={props.disabled} onClick={props.onPress}>{props.children}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export function RedButton(props) {
+    return (
+        <TouchableOpacity>
+            <Text type="submit" style={[styles.red, styles.myButton]} disabled={props.disabled} onClick={props.onPress}>{props.children}</Text>
+        </TouchableOpacity>
+    )
+}
---
+++ src/components/controls/Buttons.styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+
+    green: {
+        backgroundColor: '#00c774',
+    },
+
+    blue: {
+        backgroundColor: '#1a75ff',
+    },
+
+    red: {
+        backgroundColor: '#ff3333',
+    },
+
+    myButton: {
+        width: 80,
+        textAlign: 'center',
+        paddingBottom: 6,
+        paddingTop: 6,
+        borderRadius: 8,
+        fontSize: 16,
+        borderColor: '#5c5c5c',
+        borderWidth: 2,
+        color: 'white'
+    },
+});
