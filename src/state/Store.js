149b6304b935302e81d8444bd85e7b5d22bc80dd
Jon Graf <jongraf1@gmail.com>
2022-07-04 11:00:39 -0500

    updated state objects to better align with extensibility with device registrations down the road

--- src/state/Store.js
+++ src/state/Store.js
@@ -5,15 +5,17 @@ import Reducer from './Reducer'
 const initialState = {
     activePage: null,
     auth: {
-        bearer: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7InVzZXJfaWQiOiJlOTdmZWJjMC1mZDEwLTExZTktOGYwYi0zNjJiOWUxNTU2NjciLCJyb2xlcyI6W3siaXBfYWRkcmVzcyI6IjEuMS4xLjEiLCJyb2xlX25hbWUiOiJnYXJhZ2VfZG9vciIsImRldmljZV9pZCI6ImE3OWE4NTIwLTcxMjctNDgwMi1hODRhLTE2ZWMxOTQ1YWRjMyIsImRldmljZXMiOlt7Im5vZGVfZGV2aWNlIjoxLCJub2RlX25hbWUiOiJKb25zIn0seyJub2RlX2RldmljZSI6Miwibm9kZV9uYW1lIjoiS2FseW5ucyJ9XX0seyJyb2xlX25hbWUiOiJzdW1wX3B1bXAifSx7InJvbGVfbmFtZSI6InRoZXJtb3N0YXQifSx7InJvbGVfbmFtZSI6InNlY3VyaXR5In0seyJyb2xlX25hbWUiOiJsaWdodGluZyJ9XSwiZmlyc3RfbmFtZSI6IkpvbiIsImxhc3RfbmFtZSI6IlRlc3RlciJ9LCJyZWZyZXNoX3Rva2VuIjoiZThjNzk1NjAtMjAzNy00Nzg0LThmZDctODMzNmMzOThkZDk1IiwiZXhwIjoxNjU1OTk1MzU4fQ.crM3Q1m7EOJbZCts001ogzh3pvlMbv1ERAC5Zof-g5U',
+        bearer: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7InVzZXJfaWQiOiJlOTdmZWJjMC1mZDEwLTExZTktOGYwYi0zNjJiOWUxNTU2NjciLCJyb2xlcyI6W3siaXBfYWRkcmVzcyI6IjEuMS4xLjEiLCJyb2xlX25hbWUiOiJnYXJhZ2VfZG9vciIsImRldmljZV9pZCI6IjlkNDkxY2EwLTY5OWUtNGQ0Mi1iYjliLTc3YTdkNTE1YTRhOSIsImRldmljZXMiOlt7Im5vZGVfZGV2aWNlIjoxLCJub2RlX25hbWUiOiJKb24ncyBEb29yICJ9LHsibm9kZV9kZXZpY2UiOjIsIm5vZGVfbmFtZSI6IkthbHlubidzIERvb3IgIn1dfSx7InJvbGVfbmFtZSI6InRoZXJtb3N0YXQifSx7InJvbGVfbmFtZSI6InN1bXBfcHVtcCJ9LHsicm9sZV9uYW1lIjoic2VjdXJpdHkifSx7InJvbGVfbmFtZSI6ImxpZ2h0aW5nIn1dLCJmaXJzdF9uYW1lIjoiSm9uIiwibGFzdF9uYW1lIjoiVGVzdGVyIn0sInJlZnJlc2hfdG9rZW4iOiIxYjg1YTA1OC0wMzQzLTRkZGItOGU2YS03NDFkNzZmZTI4NTMiLCJleHAiOjE2NTY5OTI2NTF9.6d7SxPKe2N-vJDbiwxy-J8323_78V0KqmhyRvQH3rLs',
         refresh: null,
         isAuthenticated: false
     },
-    user: { userId: 'e97febc0-fd10-11e9-8f0b-362b9e155667', firstName: 'Jon', lastName: 'Graf', roles: [] },
+    user: { 
+        userId: 'e97febc0-fd10-11e9-8f0b-362b9e155667', 
+        firstName: 'Jon', 
+        lastName: 'Graf', 
+        roles: [] 
+    },
     // deviceId: 'a79a8520-7127-4802-a84a-16ec1945adc3',
-    // startedGarageRegistration: false,
-    //TODO: make this an object with different device types!!!
-    // devicesToRegister: false,
     devicesToRegister: {
         garage: {
             newDevice: true,
@@ -21,23 +23,6 @@ const initialState = {
             deviceId: 'a79a8520-7127-4802-a84a-16ec1945adc3',
         }
     },
-    //TODO: no reason for the roles to return the device data.  Put it as seperate field in claim data
-    garageRole: {
-        "ip_address": "192.168.1.175",
-        "role_name": "garage_door",
-        // "device_id": "fd8c245a-259b-47b3-be2d-ad33917c0c38",
-        // "devices": [
-            //   {
-            //     "node_device": 1,
-            //     "node_name": "Jons"
-            //   },
-            //   {
-            //     "node_device": 2,
-            //     "node_name": "Kalynns"
-            //   }
-        // ]
-    },
-    // addedGarageNode: false,
     garageCoords: null,
     garageDoors: [
         // {doorId: 1, doorName: "Jons", isOpen: true }, {doorId: 2, doorName: "Kalynns", isOpen: true }
