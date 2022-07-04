dc407f78605ef3caf94c811993a3a379cf97c710
Jon Graf <jongraf1@gmail.com>
2022-07-04 17:08:36 -0500

    mostly working version of the light switch is now pulled over

---
+++ src/components/controls/LightSwitch.styles.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+    lightGroup: {
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+
+    lightGroupExpansion: {
+        borderColor: 'darkgray',
+        borderRadius: 1,
+    },
+
+    lightSpacerOne: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        paddingLeft: 24,
+        paddingRight: 24,
+    },
+
+    lightSpacerTwo: {
+        padding: 16,
+    },
+
+    // lightTextSmall: {
+    //     fontSize: 0.7,
+    //     width: 70,
+    //     // overflow: 'hidden',
+    //     // textOverflow: 'ellipsis',
+    //     // whiteSpace: 'nowrap',
+    //     marginTop: 0.5,
+    //     marginBottom: 0.5,
+    //     marginLeft: 0.2,
+    //     marginRight: 0.2,
+    // },
+
+    brightnessIcon: {
+        fontSize: 16,
+        paddingTop: 8,
+        paddingBottom: 8,
+        paddingRight: 8,
+        paddingLeft: 8,
+    },
+
+    lightButton: {
+        borderColor: 'lightgrey',
+        borderWidth: 1,
+        borderRadius: 10,
+        alignItems: 'center',
+        alignSelf: 'center',
+        alignContent: 'center',
+        justifyContent: 'center'
+    },
+
+    lightText: {
+        padding: 6,
+        // flex: 1,
+        width: 70,
+        fontSize: 14,
+        // marginTop: 0.5,
+        // marginBottom: 0.5,
+        // marginLeft: 0.2,
+        // marginRight: 0.2,
+    },
+});
