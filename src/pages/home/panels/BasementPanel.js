764d86bb1b2f63f9c294a91edff716ff5c8abdc4
Jon Graf <jongraf1@gmail.com>
2022-07-04 12:12:58 -0500

    removed unnecessary import and data ids

--- src/pages/home/panels/BasementPanel.js
+++ src/pages/home/panels/BasementPanel.js
@@ -1,16 +1,14 @@
+import React, { useContext, useState } from 'react';
+import { Image, Text, View } from 'react-native';
+import Accordian from '../../../components/Accordion';
 import BasementIcon from '../../../resources/panelIcons/BasementIcon.png';
 import SumpPumpHighIcon from '../../../resources/panelIcons/SumpPumpHighIcon.png';
 import SumpPumpLowIcon from '../../../resources/panelIcons/SumpPumpLowIcon.png';
 import SumpPumpMediumHighIcon from '../../../resources/panelIcons/SumpPumpMediumHighIcon.png';
 import SumpPumpMediumLowIcon from '../../../resources/panelIcons/SumpPumpMediumLowIcon.png';
-
-
-import React, { useContext, useState } from 'react';
-import { Text, View, Image } from 'react-native';
-import Accordian from '../../../components/Accordion';
 import { Context } from '../../../state/Store';
-import { Divider } from '@react-native-material/core';
 import styles from './BasementPanel.styles';
+
 
 export default function BasementPanel() {
     const [open, setOpen] = useState(false);
@@ -18,13 +16,13 @@ export default function BasementPanel() {
 
     const getSumpIcon = () => {
         if (state.sumpData.warningLevel === 0) {
-            return <Image data-testid={"warning-low"} alt="sump pump" style={styles.sumpIcon} source={SumpPumpLowIcon} label="warning-low" />
+            return <Image alt="sump pump" style={styles.sumpIcon} source={SumpPumpLowIcon} label="warning-low" />
         } else if (state.sumpData.warningLevel === 1) {
-            return <Image data-testid={"warning-medium-low"} alt="sump pump" style={styles.sumpIcon} source={SumpPumpMediumLowIcon} label="warning-medium-low" />
+            return <Image alt="sump pump" style={styles.sumpIcon} source={SumpPumpMediumLowIcon} label="warning-medium-low" />
         } else if (state.sumpData.warningLevel === 2) {
-            return <Image data-testid={"warning-medium-high"} alt="sump pump" style={styles.sumpIcon} source={SumpPumpMediumHighIcon} label="warning-medium-high" />
+            return <Image alt="sump pump" style={styles.sumpIcon} source={SumpPumpMediumHighIcon} label="warning-medium-high" />
         } else if (state.sumpData.warningLevel === 3) {
-            return <Image data-testid={"warning-high"} alt="sump pump" style={styles.sumpIcon} source={SumpPumpHighIcon} label="warning-high" />
+            return <Image alt="sump pump" style={styles.sumpIcon} source={SumpPumpHighIcon} label="warning-high" />
         }
     }
 
@@ -43,7 +41,6 @@ export default function BasementPanel() {
                     }
                 </View>
             </View>
-            <Divider />
             <View style={styles.sumpGroup}>
                 {getSumpIcon()}
                 <View className={styles.sumpMeasureGroup}>
