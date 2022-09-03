db980b42dc6b679c0a548c2caef4732bf1cf4aa4
jon graf <jgraf@vermeer.com>
2022-09-03 12:57:12 -0500

    created a routes page so I navigate and use global state to see if logged in

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 // import { useColorScheme } from "react-native";
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import "react-native-gesture-handler";
-import Home from "./src/pages/home/Home";
-import Settings from "./src/pages/settings/settings";
 import Store from './src/state/Store';
+import Routes from "./src/routes/routes";
 
-
-const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-    <Store>
-      <NavigationContainer>
-        <Drawer.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
-          <Drawer.Screen name="Home" component={Home} />
-          <Drawer.Screen name="Settings" component={Settings} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </Store>
-  );
+    return (
+        <Store>
+            <Routes />
+        </Store>
+    );
 }
---
+++ src/routes/routes.js
@@ -0,0 +1,28 @@
+import React, {useContext} from 'react';
+import {NavigationContainer} from "@react-navigation/native";
+import Login from "../pages/Login/login";
+import Home from "../pages/home/Home";
+import Settings from "../pages/settings/settings";
+import {Context} from "../state/Store";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+
+
+export default function Routes() {
+    const [state,] = useContext(Context);
+    const Drawer = createDrawerNavigator();
+
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+                {state.auth.isAuthenticated
+                    ? <>
+                        <Drawer.Screen name="Home" component={Home}/>
+                        <Drawer.Screen name="Settings" component={Settings}/>
+                      </>
+                    : <Drawer.Screen name="Login" component={Login}/>
+
+                }
+            </Drawer.Navigator>
+        </NavigationContainer>
+    )
+}
