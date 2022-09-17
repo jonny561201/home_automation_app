0d6e6b5d8f1365bc631da0033d415c567f494ebc
jon graf <jgraf@vermeer.com>
2022-09-17 13:27:18 -0500

    added lighting panel styles

--- src/pages/home/lighting/lighting-panel.styles.js
+++ src/pages/home/lighting/lighting-panel.styles.js
@@ -2,6 +2,15 @@ import { StyleSheet } from 'react-native';
 
 
 export default StyleSheet.create({
+
+    lightingPanel: {
+        elevation: 5,
+        borderRadius: 8,
+        shadowOffset: { width: -2, height: 4 },
+        shadowOpacity: 0.3,
+        shadowRadius: 8,
+    },
+
     lightPanelGroup: {
         paddingTop: 8,
         paddingBottom: 8,
