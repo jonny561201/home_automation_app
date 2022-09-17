a6c1c3457d46956f16edd464f5e5ecc66748e0d3
jon graf <jgraf@vermeer.com>
2022-09-17 13:08:27 -0500

    fixed accordion naming

--- src/pages/home/lighting/lighting-panel.js
+++ src/pages/home/lighting/lighting-panel.js
@@ -1,25 +1,25 @@
+import React, { useContext, useState } from 'react';
 import LightSwitch from '../../../components/controls/light-switch';
 import LightingIcon from '../../../resources/panelIcons/LightingIcon.png';
-
-import React, { useContext, useState } from 'react';
+import Accordion from '../../../components/accordion';
 import { Context } from '../../../state/store';
 import { View, Text, Image } from 'react-native';
 import styles from './lighting-panel.styles';
-import Accordian from '../../../components/accordion';
+
 
 export default function LightingPanel() {
     const [state,] = useContext(Context);
     const [open, setOpen] = useState(false);
 
     const renderGroups = () => {
-        if (state.lights && state.lights.length) {
+        if (state.lights.length) {
             return state.lights.map(group => <LightSwitch key={`switch-${group.groupId}`} data={group} />)
         }
         return <Text>No Light Groups were found</Text>
     };
 
     return (
-        <Accordian style={styles.lightingPanel} onPress={() => { setOpen(!open) }}>
+        <Accordion style={styles.lightingPanel} onPress={() => { setOpen(!open) }}>
             <View style={styles.titleGroup}>
                 <Image style={styles.iconImage} source={LightingIcon} />
                 <Text style={[styles.panelText, styles.panelHeaderText]}>Lighting</Text>
@@ -29,6 +29,6 @@ export default function LightingPanel() {
                     {renderGroups()}
                 </View>
             </View>
-        </Accordian>
+        </Accordion>
     );
 }
