8503816c42e0c4936c2a413424b5d7fcf8ba37d4
Jon Graf <jongraf1@gmail.com>
2023-01-03 23:06:54 -0600

    create child account page will now correctly allow user to check roles they want to assign from a subset of their own roles

--- src/pages/account/create-child-account.js
+++ src/pages/account/create-child-account.js
@@ -1,29 +1,24 @@
 import React, { useContext, useState } from 'react';
 import { addUserChildAccount } from '../../utilities/rest-api';
-import { Dialog, Paragraph, TextInput } from 'react-native-paper';
-import { View, Text } from 'react-native';
-import { BlueButton, GreenButton, RedButton } from '../../components/controls/buttons';
+import { Dialog, TextInput, Checkbox } from 'react-native-paper';
+import { GreenButton, RedButton } from '../../components/controls/buttons';
 import { Context } from "../../state/store";
 
 
 export default function CreateChildAccount(props) {
-    const [state, dispatch] = useContext(Context);
-    const [open, setOpen] = useState(false);
-    const [roles, setRoles] = useState([]);
-    const [role, setRole] = useState('');
+    const [state, _] = useContext(Context);
+    const [roles, setRoles] = useState(state.user.roles.map(x => ({...x, checked: false})));
     const [email, setEmail] = useState('');
     const [isEmailInvalid, setIsEmailInvalid] = useState(undefined);
-    const [isRoleInvalid, setIsRoleInvalid] = useState(undefined);
 
     const submitChildAccount = async () => {
-        if ((!isEmailInvalid && !isRoleInvalid) && (selectedRole.length !== 0 && email !== null && email !== "")) {
-            const response = await addUserChildAccount(state.user.userId, state.auth.bearer, email, selectedRole);
-            setChildAccounts(response);
+        if ((!isEmailInvalid) && (roles !== 0 && email !== null && email !== "")) {
+            const selectedRoles = roles.filter(x => x.checked).map(y => y.role_name);
+            const response = await addUserChildAccount(state.user.userId, state.auth.bearer, email, selectedRoles);
+            props.addChild(response);
             setEmail("");
-            setSelectedRole([]);
         } else {
             setIsEmailInvalid(email === "" || email === null);
-            setIsRoleInvalid(selectedRole.length === 0);
         }
     }
 
@@ -32,9 +27,8 @@ export default function CreateChildAccount(props) {
         setIsEmailInvalid(value === "");
     }
 
-    const validateRole = (value) => {
-        setSelectedRole(value);
-        setIsRoleInvalid(value === "");
+    const updateRole = (role) => {
+        setRoles(roles.map(x => (x.role_name === role.role_name) ? {...role, checked: !role.checked} : x));
     }
 
     return (
@@ -42,29 +36,15 @@ export default function CreateChildAccount(props) {
             <Dialog.Title>Add User</Dialog.Title>
             <Dialog.Content>
                 <TextInput value={email} error={isEmailInvalid} onChangeText={(input) => validateEmail(input)} mode='outlined' activeOutlineColor='#00c774' label="Email" />
+                {
+                    roles.map(x =>  <Checkbox.Item key={x.role_name} label={x.role_name} status={x.checked ? 'checked' : 'unchecked'} onPress={() => updateRole(x)}/> )
+                }
             </Dialog.Content>
 
             <Dialog.Actions>
                 <RedButton onPress={props.close}>Cancel</RedButton>
-                <GreenButton onPress={() => console.log('Pressed')}>Add</GreenButton>
+                <GreenButton onPress={submitChildAccount}>Add</GreenButton>
             </Dialog.Actions>
-
-
-            {/* <FormControl error={isRoleInvalid}>
-                <InputLabel>Roles</InputLabel>
-                <Select multiple value={selectedRole} onChange={(input) => validateRole(input)} input={<Input />}
-                    renderValue={(selectedRole) => (selectedRole.join(', '))}>
-                    {roles.map((role) => (
-                        <MenuItem key={role.role_name} value={role.role_name}>
-                            <Checkbox checked={selectedRole.indexOf(role.role_name) > -1} />
-                            <ListItemText primary={role.role_name} />
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <View style={styles.tableEndItem}>
-                <GreenButton onPress={submitChildAccount} />
-            </View> */}
         </>
     )
 }
