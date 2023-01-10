3c386ac3964c23272769e06021abcdde0fe976b1
Jon Graf <jongraf1@gmail.com>
2023-01-09 18:21:34 -0600

    got the edit menu now pulling up the dialog with the correct roles passed in

--- src/pages/account/account-child-user.js
+++ src/pages/account/account-child-user.js
@@ -1,15 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { deleteUserChildAccount } from '../../utilities/rest-api';
 import { Context } from '../../state/store';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';
 import { List } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { SwipeListView } from 'react-native-swipe-list-view';
+import { Portal, Dialog } from 'react-native-paper';
+import CreateChildAccount from './create-child-account';
 import styles from './account-child-user.styles';
 
 
 export default function AccountChildUser(props) {
     const [state, _] = useContext(Context);
+    const [display, setDisplay] = useState(false);
+    const [roles, setRoles] = useState([]);
 
     const deleteChildUser = async (childUser) => {
         const response = await deleteUserChildAccount(state.user.userId, state.auth.bearer, childUser.user_id);
@@ -30,16 +34,26 @@ export default function AccountChildUser(props) {
         return rowStyles;
     }
 
+    const closeDialog = () => setDisplay(false);
+
+    const setChildAccounts = () => { console.log('tried setting child account') }
+
+    const edit = (child) => {
+        setDisplay(true);
+        setRoles(child?.item?.roles);
+    }
+
     return (
-        <>
+        <View style={{ flex: 1, paddingRight: 10 }}>
             <Text style={styles.accountHeader}>Account Users</Text>
-            <List.Section>
-                <SwipeListView
-                    data={props.childAccounts}
-                    rightOpenValue={-150}
-                    disableRightSwipe
-                    previewRowKey={'0'}
-                    renderItem={(data) => (
+            <SwipeListView
+                data={props.childAccounts}
+                rightOpenValue={-150}
+                disableRightSwipe
+                previewRowKey={'0'}
+                style={{marginBottom: 20}}
+                renderItem={(data) => (
+                    <>
                         <List.Accordion
                             key={`user-${data.item.user_name}`}
                             theme={{ colors: { primary: '#00c774' } }}
@@ -60,19 +74,25 @@ export default function AccountChildUser(props) {
                                 })
                             }
                         </List.Accordion>
-                    )}
-                    renderHiddenItem={(data) => (
-                        <View style={styles.swipeContainer}>
-                            <TouchableOpacity style={styles.swipeEdit} onPress={() => {console.log('test')}}>
-                                <Icon name='edit' size={30} style={styles.swipeText} />
-                            </TouchableOpacity>
-                            <TouchableOpacity style={styles.swipeDelete} onPress={() => deleteChildUser(data.item)}>
-                                <Icon name='delete' size={30} style={styles.swipeText} />
-                            </TouchableOpacity>
-                        </View>
-                    )}
-                />
-            </List.Section>
-        </>
+                    </>
+                )}
+                renderHiddenItem={(data) => (
+                    <View style={styles.swipeContainer}>
+                        <TouchableOpacity style={styles.swipeEdit} onPress={() => edit(data)}>
+                            <Icon name='edit' size={30} style={styles.swipeText} />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.swipeDelete} onPress={() => deleteChildUser(data.item)}>
+                            <Icon name='delete' size={30} style={styles.swipeText} />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+
+            <Portal>
+                <Dialog visible={display} onDismiss={closeDialog}>
+                    <CreateChildAccount close={closeDialog} addChild={setChildAccounts} roles={roles} />
+                </Dialog>
+            </Portal>
+        </View>
     );
 }
