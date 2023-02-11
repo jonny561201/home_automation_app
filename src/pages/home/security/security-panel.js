668c8d12b069df0808e3605c1021388b41b55afc
Jon Graf <jongraf1@gmail.com>
2023-02-11 16:23:23 -0600

    updated the security panel to use the theme colors to support dark mode

--- src/pages/home/security/security-panel.js
+++ src/pages/home/security/security-panel.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useTheme } from 'react-native-paper';
 import { Image, Text, View } from 'react-native';
 import Accordion from '../../../components/accordion';
 import SecurityIcon from '../../../resources/panelIcons/SecurityIcon.png';
@@ -6,6 +7,7 @@ import styles from './security-panel.styles';
 
 
 export default function SecurityPanel() {
+    const theme = useTheme();
     const [open, setOpen] = useState(false);
 
     return (
@@ -13,12 +15,12 @@ export default function SecurityPanel() {
             <View style={styles.titleGroup}>
                 <Image style={styles.iconImage} source={SecurityIcon} />
                 <View style={styles.securityHeader}>
-                    <Text style={styles.statusTextBold}>Security</Text>
+                    <Text style={[styles.statusTextBold, {color: theme.colors.font}]}>Security</Text>
                 </View>
             </View>
             <View style={styles.securityGroup}>
                 <View style={styles.smallTextGroup}>
-                    <Text>Test Text for Now</Text>
+                    <Text style={{color: theme.colors.font}}>Test Text for Now</Text>
                 </View>
             </View>
         </Accordion >
