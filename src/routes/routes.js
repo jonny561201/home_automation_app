edbee6fc7d92b1ee43c5262bafc227a98b0bed38
Jon Graf <jongraf1@gmail.com>
2023-02-14 18:54:36 -0600

    added icons to the navigation page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,6 +9,7 @@ import { Context } from "../state/store";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Account from "../pages/account/account";
 import Events from '../pages/events/events';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 export default function Routes() {
@@ -18,15 +19,31 @@ export default function Routes() {
 
     return (
         <NavigationContainer theme={theme}>
-            <Drawer.Navigator screenOptions={{headerShown: false}} initialRouteName="Home" drawerContent={LogoutDrawer}>
+            <Drawer.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home" drawerContent={LogoutDrawer}>
                 {state.auth.isAuthenticated
                     ? <>
-                        <Drawer.Screen name="Home" component={Home}/>
-                        <Drawer.Screen name="Events" component={Events}/>
-                        <Drawer.Screen name="Settings" component={Settings}/>
-                        <Drawer.Screen name="Account" component={Account}/>
-                      </>
-                    : <Drawer.Screen name="Login" component={Login}/>
+                        <Drawer.Screen
+                            name="Home"
+                            component={Home}
+                            options={{ drawerIcon: ({ focused }) => (<Icon name="home" size={20} color={focused ? theme.colors.primary : theme.colors.font} />) }}
+                        />
+                        <Drawer.Screen
+                            name="Events"
+                            component={Events}
+                            options={{ drawerIcon: ({ focused }) => (<Icon name="event" size={20} color={focused ? theme.colors.primary : theme.colors.font} />) }}
+                        />
+                        <Drawer.Screen
+                            name="Settings"
+                            component={Settings}
+                            options={{ drawerIcon: ({ focused }) => (<Icon name="settings" size={20} color={focused ? theme.colors.primary : theme.colors.font} />) }}
+                        />
+                        <Drawer.Screen
+                            name="Account"
+                            component={Account}
+                            options={{ drawerIcon: ({ focused }) => (<Icon name="account-circle" size={20} color={focused ? theme.colors.primary : theme.colors.font} />) }}
+                        />
+                    </>
+                    : <Drawer.Screen name="Login" component={Login} />
                 }
             </Drawer.Navigator>
         </NavigationContainer>
