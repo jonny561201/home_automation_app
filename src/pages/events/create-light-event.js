eba1fbbe93b7fb94bc30f8547d741dc4b329e403
Jon Graf <jongraf1@gmail.com>
2023-02-27 19:20:01 -0600

    created initial component to display the options when trying to create a new light event

---
+++ src/pages/events/create-light-event.js
@@ -0,0 +1,84 @@
+import React, { useState, useContext } from 'react';
+import { Context } from '../../state/store';
+import { View } from "react-native"
+import moment from 'moment/moment';
+import { Picker } from '@react-native-picker/picker';
+import { useTheme, TextInput, TouchableRipple } from 'react-native-paper';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import styles from './create-light-event.sytles'
+import WeekPicker from '../../components/controls/week-picker';
+
+
+export default function CreateLightEvent() {
+    const initialDays = [{ id: 'Sun', day: 'S', on: false }, { id: 'Mon', day: 'M', on: false }, { id: 'Tue', day: 'T', on: false }, { id: 'Wed', day: 'W', on: false }, { id: 'Thu', day: 'T', on: false }, { id: 'Fri', day: 'F', on: false }, { id: 'Sat', day: 'S', on: false }];
+    const theme = useTheme();
+    const [state, dispatch] = useContext(Context);
+    const [days, setDays] = useState();
+    const [open, setOpen] = useState(false);
+    const [edited, setEdited] = useState(false);
+    const [selectedRoom, setSelectedRoom] = useState();
+    const [daysOfWeek, setDaysOfWeek] = useState(initialDays);
+    const [selectedTime, setSelectedTime] = useState(new Date());
+    const [value, setValue] = useState(moment(selectedTime).format('hh:mm a'));
+
+    const handleConfirm = (date) => {
+        setSelectedTime(date);
+        setOpen(false);
+      };
+
+      const toggleDay = (task, newState) => {
+        const newProjects = daysOfWeek.map(day => day.id === task.id
+            ? { ...day, on: newState }
+            : day
+        );
+        setDaysOfWeek(newProjects);
+        setDays(newProjects.filter(x => x.on === true).map(y => y.id).join(''));
+    }
+
+    const handleChange = (newValue) => {
+        console.log(moment(newValue).format('hh:mm a'))
+    }
+
+    return (
+        <>
+        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+            <View style={[styles.pickerContainer, {borderColor: theme.colors.secondaryFont}]}>
+                <Picker
+                    style={[styles.picker, { backgroundColor: theme.colors.background, color: theme.colors.secondaryFont }]}
+                    selectedValue={selectedRoom}
+                    onValueChange={setSelectedRoom}>
+                    <Picker.Item label='None' value='None' style={{ backgroundColor: theme.colors.background, color: theme.colors.secondaryFont }} />
+                    {
+                        state.lights.map(x => {
+                            return <Picker.Item key={x.groupId} label={x.groupName} value={x.groupName} style={{ backgroundColor: theme.colors.background, color: theme.colors.secondaryFont }} />
+                        })
+                    }
+                </Picker>
+            </View>
+                <TextInput
+                    // style={{ borderColor: 'white' }}
+                    value={value}
+                    // disabled='true'
+                    // editable='false'
+                    outlineColor={theme.colors.primaryFont}
+                    // error={isEmailInvalid}
+                    // onChangeText={setTime}
+                    onChange={handleChange}
+                    mode='outlined'
+                    textColor={theme.colors.secondaryFont}
+                    activeOutlineColor={theme.colors.primary}
+                    right={<TextInput.Icon icon='clock-outline' color={theme.colors.primaryFont} onPress={() => setOpen(true)} />}
+                    label="Time" />
+        </View>
+                <WeekPicker daysOfWeek={initialDays} toggleDay={toggleDay} setEdited={() => setEdited(true)}/>
+
+                <DateTimePickerModal
+                    date={selectedTime}
+                    isVisible={open}
+                    mode="time"
+                    onConfirm={handleConfirm}
+                    onCancel={() => setOpen(false)}
+                />
+        </>
+    )
+}
---
+++ src/pages/events/create-light-event.sytles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+
+    pickerContainer: {
+        borderRadius: 4,
+        borderWidth: 1,
+        width: '52%',
+        margin: 10,
+    },
+
+    picker: {
+        // width: '50%'
+    },
+});
