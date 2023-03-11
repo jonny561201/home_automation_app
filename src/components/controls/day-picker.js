6e4e6ee68f9baa1ab752d3f5afb784b9b6cbeb0e
Jon Graf <jongraf1@gmail.com>
2023-03-11 14:47:42 -0600

    updated the day pickers to use the touchable opacity

--- src/components/controls/day-picker.js
+++ src/components/controls/day-picker.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-import { TouchableRipple } from 'react-native-paper';
+import { Text, View, TouchableOpacity } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import styles from './day-picker.styles';
 
@@ -18,13 +17,13 @@ export default function DayPicker(props) {
 
     return (
         <View style={styles.dayPicker}>
-            <TouchableRipple
+            <TouchableOpacity
                 style={[styles.dayPicker, on ? { backgroundColor: '#E0E0E0' } : { backgroundColor: theme.colors.primary }]}
                 onPress={toggleDay}
                 borderless={true}
                 rippleColor='#4ce0a3'>
                 <Text style={[{textAlign: 'center', fontWeight: 'bold', fontSize: 18 }, on ? {color: theme.colors.primary} : { color: theme.colors.primaryFont}]}>{props.day.day}</Text>
-            </TouchableRipple>
+            </TouchableOpacity>
         </View>
     )
 }
